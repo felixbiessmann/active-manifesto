5f7ba7d9fd3c282d84948bfe8c9cd743597f701f
philipp <tdhd@users.noreply.github.com>
2017-11-04 11:52:54 -0700

    submit label button now working

--- services/user_interface/main.py
+++ services/user_interface/main.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from flask import Flask
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 import json
 
 
@@ -9,6 +9,16 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
 
+
+@app.route('/user_labels', methods=['POST'])
+def user_labels():
+    # fixme: check bytes encoding here
+    # todo: forward to model http facade and receive bias estimation
+    labels = json.loads(request.data.decode('utf-8'))
+    print(labels)
+    for entry in labels['data']:
+        print(entry)
+    return '{}'
 
 @app.route('/get_samples')
 def get_samples():
