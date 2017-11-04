186d3f0582d945705be24eb3f578cccbc4118244
philipp <tdhd@users.noreply.github.com>
2017-11-04 05:18:57 -0700

    get samples calls user interface endpoint and displays results

--- services/user_interface/main.py
+++ services/user_interface/main.py
@@ -2,15 +2,30 @@
 
 from flask import Flask
 from flask import render_template, redirect, url_for
+import json
+
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
 
 
+@app.route('/get_samples')
+def get_samples():
+    # todo: call model http facade here
+    response = {
+        'samples': [
+            {'text': 'abc'},
+            {'text': 'abcd'}
+        ]
+    }
+    return json.dumps(response)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
