86b257366ea2e78ab5fbf5f57cd36eed1b202627
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-11-05 22:48:23 +0100

    Some more API methods
     - changed user_labels to submit_labels, not implemented yet, need to think about how to compute bias
     - made get_samples a POST, to provide number of requested texts
     - still no DB connection

--- services/manifesto_model/api.py
+++ services/manifesto_model/api.py
@@ -2,7 +2,7 @@
 import flask
 from flask import Flask, request, jsonify
 import os
-from classifier import Classifier
+from classifier import Classifier, load_data
 # from apscheduler.schedulers.background import BackgroundScheduler
 import json
 
@@ -25,7 +25,26 @@ def predict():
     text = request.form['text']
     return jsonify(classifier.predict(text))
 
+@app.route("/get_samples", methods=['POST'])
+def get_samples():
+    # TODO: wasn't sure how to deal with number of requested samples - should this be a GET?
+    n_samples = int(request.form['samples'])
+    # TODO: replace data loading with DB data loader
+    texts,_ = load_data()
+    texts = classifier.prioritize(texts)
+    response = {'samples': [{'text':t} for t in texts[:n_samples]]}
+    return jsonify(response)
+
+@app.route("/submit_labels", methods=['POST'])
+def submit_labels():
+    user_id = request.form['user']
+    # data = request.form['data']
+    # TODO: compute left/right bias
+    print(user_id)
+    return jsonify({'user_bias': user_id})
+
+
 if __name__ == "__main__":
     port = 5000
-    classifier = retrain()
+    classifier = Classifier(train=False)
     app.run(host='0.0.0.0', port = port, debug = DEBUG)
