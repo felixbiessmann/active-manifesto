e0bad62e7362b1e87e10c935ac06ed4078775b4a
philipp <tdhd@users.noreply.github.com>
2017-11-09 11:24:12 -0800

    prune containers in run script

--- services/run.sh
+++ services/run.sh
@@ -2,9 +2,14 @@
 
 sudo docker rmi services_user_interface -f
 sudo docker rmi services_persistence -f
-#sudo docker container prune
+sudo docker container prune
 
-export WZB_API_KEY=abc
+echo ""
+
+export WZB_API_KEY=""
+#export HOST_DB_PATH="./db/active-manifesto.db"
+#export CONTAINER_DB_PATH="/db/active-manifesto.db"
+
 python data_import.py
-sudo docker-compose up
 
+sudo docker-compose up
