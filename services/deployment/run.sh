1c2f2495ba1ae49277e9644cfbface31db3e3ba4
Felix Biessmann <felix.biessmann@tu-berlin.de>
2018-01-12 00:35:27 +0100

    fixed some bugs

--- services/deployment/run.sh
+++ services/deployment/run.sh
@@ -2,7 +2,7 @@
 
 # sudo docker stop $(sudo docker ps -a -q)
 # sudo docker rm $(sudo docker ps -a -q)
-#
+# #
 # sudo docker rmi services_user_interface -f
 # sudo docker rmi services_manifesto_model -f
 # sudo docker rmi news_crawler -f
@@ -14,7 +14,5 @@ echo ""
 export WZB_API_KEY="9cd9104a725f26bcae04da3eed6bdd40"
 
 python data_import.py
-
-
 
 sudo docker-compose up
